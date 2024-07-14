adf0b6c53ea464e4825f5d4e1e035459b9fcf723
akilkhatri104 <84614450+akilkhatri104@users.noreply.github.com>
2024-07-14 04:44:22 +0000

    day2 complete

---
+++ day2.js
@@ -0,0 +1,99 @@
+//Day 2 - Operators
+
+//Activity-1 Arithmetic Operators
+
+//Task-1:Write a program to add two numbers and log the result
+// let num1 = 6
+// let num2 = 7
+// console.log(num1+num2);
+
+//Task-2:Write a program to subtract two numbers and log the result
+// let num1 = 4
+// let num2 = 8
+// console.log(num1-num2);
+
+// Task-3:Write a program to multiply two numbers and log the result
+// let num1 = 5
+// let num2 = 8
+// console.log(num1*num2);
+
+//Task-4:Write a program to divide two numbers and log the result
+// let num1 = 4
+// let num2 = 2
+// if (num2 == 0) {
+//     console.log("Division by 0 not possible");
+// }else
+// console.log(num1/num2);
+
+//Task-5:Write a program to get remainder two numbers and log the result
+// let num1 = 4
+// let num2 = 4
+// if (num2 == 0) {
+//     console.log("Division by 0 not possible");
+// }else
+// console.log(num1%num2);
+
+
+//Activity-2: Assignment Operators
+
+//Task-6:Use the += operator to add a number to a variable and log the result
+// let n = 9
+// n += 2
+// console.log(n);
+
+//Task-7:Use the -= operator to subtract a number to a variable and log the result
+// let n = 9
+// n -= 2
+// console.log(n);
+
+//Activity-3: Comparison Operator
+
+//Task-8:Write a program to compare two numbers using > and < and log the result
+// let num = 12
+// if (num < 15) {
+//     console.log(`${num} is less than 15`);
+// }else console.log(`${num} is not less than 15`);
+// if (num > 10) {
+//     console.log(`${num} is greater than 10`);
+// }else console.log(`${num} is not greater than 10`);
+
+//Task-9:Write a program to compare two numbers using > and < and log the result
+// let num = 15
+// if (num <= 15) {
+//     console.log(`${num} is less than or equal to 15`);
+// }else console.log(`${num} is not less than or equal to 15`);
+// if (num >= 10) {
+//     console.log(`${num} is greater than or equal to 10`);
+// }else console.log(`${num} is not greater than or equal to 10`);
+
+//Task-10:Write a program to compare two numbers using == and === and log the result
+// let n1 = 15
+// let n2 = "15"
+// if (n1 == n2) {
+//     console.log(`${n1} is equal to ${n2}`);
+// }else console.log(`${n1} is not equal to ${n2}`);
+// if (n1 === n2) {
+//     console.log(`${n1} is strictly equal to ${n2}`);
+// }else console.log(`${n1} is not strictly equal to ${n2}`);
+
+
+//Activity-4: Logical Operators
+
+//Task-11: Write a program that uses the && operator to combine two conditions and log the result to the console
+// let n1 = 25
+// if (n1 > 10 && n1 < 20) {
+//     console.log(`${n1} is between 10 and 20`);
+// }else console.log(`${n1} is not between 10 and 20`);
+
+//Task-12: Write a program that uses the || operator to combine two conditions and log the result to the console
+// let n1 = 12
+// let n2 = 2
+// console.log(n1 < 0 || n2 < 0);
+
+//Ternary Operator: checking if a no. is +ve or -ve
+let n = 0
+console.log((n>=0)?("The number is positive"):("The number is negative"));
+
+
+
+
